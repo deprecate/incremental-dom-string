31f567f2ae91a18094ca0eadd4326503a17179b7
Julien C <julien.castelain@liferay.com>
2017-03-01 18:03:43 +0100

    Initial implementation

--- src/virtual_elements.js
+++ src/virtual_elements.js
@@ -1,3 +1,32 @@
+
+let elementDummy_ = {
+  addEventListener() {}
+};
+
+let buffer_ = [];
+let output_ = {};
+let nestingCount_ = 1;
+
+const push_ = (token, close = false) => {
+  buffer_.push(token);
+
+  if (close) {
+    output_.html = buffer_.slice().join('');
+    buffer_ = [];
+  }
+};
+
+const getOutput = () => {
+  return output_.html;
+};
+
+const attrsArray_ = (data) => {
+  for (let i = 0, l = data.length; i < l; i += 2) {
+    attr(data[i], data[i + 1]);
+  }
+};
+
+
 /***
  * Defines a virtual attribute at this point of the DOM. This is only valid
  * when called between elementOpenStart and elementOpenEnd.
@@ -6,6 +35,7 @@
  * @param {*} value
  */
 const attr = function(name, value) {
+  push_(` ${name}="${value}"`);
 };
 
 /**
@@ -15,6 +45,14 @@ const attr = function(name, value) {
  * @return {!Element} The corresponding Element.
  */
 const elementClose = function(nameOrCtor) {
+  let close = false;
+  nestingCount_--;
+  if (nestingCount_ === 1) {
+    close = true;
+  }
+  push_(`</${nameOrCtor}>`, close);
+
+  return elementDummy_;
 };
 
 /**
@@ -32,7 +70,10 @@ const elementClose = function(nameOrCtor) {
  * @return {!Element} The corresponding Element.
  */
 const elementVoid = function(nameOrCtor, key, statics, var_args) {
+  elementOpen(nameOrCtor, key, statics, var_args);
+  return elementClose(nameOrCtor);
 };
+
 
 /**
  * @param {NameOrCtorDef} nameOrCtor The Element's tag or constructor.
@@ -47,6 +88,14 @@ const elementVoid = function(nameOrCtor, key, statics, var_args) {
  * @return {!Element} The corresponding Element.
  */
 const elementOpen = function(nameOrCtor, key, statics, var_args) {
+
+  elementOpenStart(nameOrCtor, key, statics);
+
+  if (Array.isArray(var_args)) {
+    attrsArray_(var_args);
+  }
+
+  return elementOpenEnd();
 };
 
 /**
@@ -54,6 +103,10 @@ const elementOpen = function(nameOrCtor, key, statics, var_args) {
  * @return {!Element} The corresponding Element.
  */
 const elementOpenEnd = function() {
+  push_('>');
+  nestingCount_++;
+
+  return elementDummy_;
 };
 
 /**
@@ -71,9 +124,16 @@ const elementOpenEnd = function() {
  *     Element is created.
  */
 const elementOpenStart = function(nameOrCtor, key, statics) {
+  push_(`<${nameOrCtor}`);
+
+  // TODO: probably do something useful with key
+  if (Array.isArray(statics)) {
+    attrsArray_(statics);
+  }
 };
 
 const patch = function() {
+
 };
 
 /**
@@ -86,6 +146,15 @@ const patch = function() {
  * @return {!Text} The corresponding text node.
  */
 const text = function(value, var_args) {
+  let formatted = value;
+
+  if (Array.isArray(var_args)) {
+    for (let v of var_args) {
+      formatted = v(formatted);
+    }
+  }
+
+  push_('' + formatted);
 };
 
 export {
@@ -96,4 +165,5 @@ export {
   elementClose,
   text,
   attr,
+  getOutput
 };
--- test/virtual_elements.js
+++ test/virtual_elements.js
@@ -7,13 +7,87 @@ import {
   elementClose,
   text,
   attr,
+  getOutput
 } from '../src/virtual_elements.js';
 
 describe('element creation', () => {
   it('when creating a single node with text', function() {
+
     elementOpen('div');
       text('Hello world');
     elementClose('div');
-    assert.strictEqual('', '<div>Hello world</div>');
+
+    assert.strictEqual(getOutput(), '<div>Hello world</div>');
+  });
+
+  it('when creating a single node with a child node with text', function() {
+    elementOpen('div');
+      elementOpen('span');
+        text('Hello world 2');
+      elementClose('span');
+    elementClose('div');
+    assert.strictEqual(getOutput(), '<div><span>Hello world 2</span></div>');
+  });
+
+  it('when creating a single node with several child nodes with text', function() {
+    elementOpen('div');
+      elementOpen('p');
+        text('First child');
+      elementClose('p');
+      elementOpen('span');
+        text('Second child');
+      elementClose('span');
+    elementClose('div');
+    assert.strictEqual(getOutput(), '<div><p>First child</p><span>Second child</span></div>');
+  });
+
+  it('when creating a single node with attributes', function() {
+    elementOpen('div', null, ['id', 'test-div']);
+      text('Test text');
+    elementClose('div');
+    assert.strictEqual(getOutput(), '<div id="test-div">Test text</div>');
+  });
+
+  it('when creating a single node several attributes', function() {
+
+    const attrs = [
+      'id', 'test-id',
+      'name', 'test-name',
+      'data-a', 'test-data-a',
+      'data-b', 'test-data-b'
+    ];
+
+    const expected = ['<div id="test-id" name="test-name"',
+      'data-a="test-data-a"',
+      'data-b="test-data-b">Some text</div>'].join(' ');
+
+    elementOpen('div', null, attrs);
+      text('Some text');
+    elementClose('div');
+    assert.strictEqual(getOutput(), expected);
+  });
+
+  it('when creating a single node with several child nodes with attributes and text', function() {
+    elementOpen('div');
+      elementOpen('span', null, ['name', 'span-name', 'id', 'span-id']);
+        text('Foo');
+      elementClose('span');
+      elementOpen('button', null, ['name', 'button-name', 'id', 'button-id']);
+        text('Bar');
+      elementClose('button');
+    elementClose('div');
+
+    const expected = ['<div>',
+      '<span name="span-name" id="span-id">Foo</span>',
+      '<button name="button-name" id="button-id">Bar</button>',
+      '</div>'].join('');
+
+    assert.strictEqual(getOutput(), expected);
+  });
+
+  it('when creating a void node', function() {
+    elementVoid('input', null, ['type', 'text']);
+
+    assert.strictEqual(getOutput(), '<input type="text"></input>');
   });
 });
